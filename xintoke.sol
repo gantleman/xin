6b2c9c59a2bf6fe1a6f734748d58a8304f768b06
gantleman <dds_sun@hotmail.com>
2018-04-17 17:49:43 +0800

    modified

--- xintoke.sol
+++ xintoke.sol
@@ -10,25 +10,53 @@ contract xintoken {
     struct repair 
     {
         string url;
+        uint price;
         mapping(address => uint) buy;
         mapping(address => rate) fallow;
     }
     mapping(address => mapping(address => repair)) public info;
     
-     function xintoken() public {
+    address admin;
+    
+    function xintoken(address padmin) public {
+         admin = padmin;
     }
     
-    function add_repair(address car, string url) public payable {
+    ///添加维保记录
+    function add_repair(address car, string url, uint price) public payable {
         info[car][msg.sender].url = url;
+        info[car][msg.sender].price = price;
     }
-    
+
+    ///添加评分
     function add_rate(address car, address sell, uint irate, string comment) public payable {
         info[car][sell].fallow[msg.sender].v_rate = irate;
         info[car][sell].fallow[msg.sender].comment = comment;
     }
 
+    ///购买维保记录
     function buy(address car, address sell) public payable returns (string url){
-        info[car][sell].buy[msg.sender]=1;
+        if(bytes(info[car][sell].url).length == 0)
+            return '';
+            
+        info[car][sell].buy[msg.sender] += msg.value;
+        if(msg.value != 0)
+            sell.transfer(msg.value * 5 / 10);
+        
         return info[car][sell].url;
+    }
+
+    ///检查是否购买过维保记录
+    function isbuy(address car, address buyer) public view returns (uint){
+        
+        return info[car][msg.sender].buy[buyer]/info[car][sell].price >= 1 ? 1:0;
+    }
+    
+    //提现接口
+    function collect() public payable{
+        if(msg.sender != admin)
+        return;
+        
+        admin.transfer(address(this).balance);
     }
 }
