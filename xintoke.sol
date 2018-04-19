6d7ae7c74323adbd0af723c9b5e9393fa12a53b6
gantleman <dds_sun@hotmail.com>
2018-04-19 14:38:28 +0800

    add getaddress

--- xintoke.sol
+++ xintoke.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.4.0;
 
 contract xintoken {
-    
+
     struct rate
     {
         uint  v_rate;//1~5
         string comment;
     }
+    
     struct repair 
     {
         string url;
@@ -16,7 +17,15 @@ contract xintoken {
         mapping(address => uint) buy;
         mapping(address => rate) fallow;
     }
-    mapping(address => mapping(address => repair)) public info;
+    
+    struct sell4s
+    {
+        uint T;
+        address[] mkeys;
+        mapping(address => repair) detailed;
+    }
+    
+    mapping(address => sell4s) public info;
     
     address admin;
     
@@ -24,32 +33,67 @@ contract xintoken {
          admin = padmin;
     }
     
-    ///添加维保记录
+ ///添加维保记录
     function add_repair(address car, string url, uint price) public payable {
-        info[car][msg.sender].url = url;
-        info[car][msg.sender].price = price;
+        info[car].detailed[msg.sender].url = url;
+        info[car].detailed[msg.sender].price = price;
+        
+        for(uint i =0; i< info[car].mkeys.length; i++)
+        {
+            if(info[car].mkeys[i] == msg.sender)
+                return;
+        }
+        info[car].mkeys[info[car].mkeys.length] = msg.sender;
     }
 
     ///添加评分
     function add_rate(address car, address sell, uint irate, string comment) public payable {
-        info[car][sell].fallow[msg.sender].v_rate = irate;
-        info[car][sell].fallow[msg.sender].comment = comment;
+        info[car].detailed[sell].fallow[msg.sender].v_rate = irate;
+        info[car].detailed[sell].fallow[msg.sender].comment = comment;
     }
 
     ///购买维保记录
     function buy(address car, address sell) public payable returns (string url){
-        if(bytes(info[car][sell].url).length == 0)
+        if(bytes(info[car].detailed[sell].url).length == 0)
             return '';
 
-        info[car][sell].buy[msg.sender] += msg.value;
-        info[car][sell].balance += msg.value * 9 /10;
+        info[car].detailed[sell].buy[msg.sender] += msg.value;
+        info[car].detailed[sell].balance += msg.value * 9 /10;
         
-        return info[car][sell].url;
+        return info[car].detailed[sell].url;
+    }
+    
+     function toString(address x) internal returns (string) {
+        bytes memory b = new bytes(20);
+        for (uint i = 0; i < 20; i++)
+            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+        return string(b);
+    }
+    
+    function strConcat(string _a, string _b) internal returns (string){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory abcde = new string(_ba.length + _bb.length);
+        bytes memory babcde = bytes(abcde);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        return string(babcde);
     }
 
+    ///获取4s车商地址
+    function getaddress(address car) public view returns (string){
+        string memory rets;
+        for(uint i =0; i< info[car].mkeys.length; i++)
+        {
+            rets = strConcat(rets, toString(info[car].mkeys[i]));
+            rets = strConcat(rets, ";");
+        }
+    }
+    
     ///检查是否购买过维保记录
     function isbuy(address car, address buyer) public view returns (uint){
-        return info[car][msg.sender].buy[buyer] / info[car][msg.sender].price >= 1 ? 1:0;
+        return info[car].detailed[msg.sender].buy[buyer] / info[car].detailed[msg.sender].price >= 1 ? 1:0;
     }
     
     //提现接口
@@ -60,15 +104,14 @@ contract xintoken {
         admin.transfer(address(this).balance);
     }
     
-    //用户自己提现的接口
     function selfcollect(address car) public payable{
-        if(info[car][msg.sender].balance == 0)
+        if(info[car].detailed[msg.sender].balance == 0)
             return;
         if(address(this).balance == 0)
             return;
             
-        if(address(this).balance >= info[car][msg.sender].balance)
-            admin.transfer(info[car][msg.sender].balance);
+        if(address(this).balance >= info[car].detailed[msg.sender].balance)
+            admin.transfer(info[car].detailed[msg.sender].balance);
         else
             admin.transfer(address(this).balance);
     }
