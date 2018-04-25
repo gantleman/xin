02ac2d02cd72418db700fe653c534a588536b741
gantleman <dds_sun@hotmail.com>
2018-04-25 19:18:46 +0800

    版本3

--- xintoke.sol
+++ xintoke.sol
@@ -1,14 +1,15 @@
 pragma solidity ^0.4.0;
+import './strings.sol';
 
 contract xintoken {
-    struct rate
-    {
+    using strings for *;
+    
+    struct rate{
         uint  v_rate;//1~5
         string comment;
     }
     
-    struct repair 
-    {
+    struct repair {
         string url;
         uint price;
         uint stamp;
@@ -21,11 +22,10 @@ contract xintoken {
     
     mapping(address => mapping(address => repair)) public info;
     mapping(address => uint) public coin;
+    address public admin;
     mapping(address => string) public index;
     
-    
-    mapping(address => mapping(address=>bool)) public mkeys;
-    address public admin;
+    int public error;
     
     function xintoken(address padmin) public {
          admin = padmin;
@@ -37,71 +37,55 @@ contract xintoken {
         coin[buyer] += value;
     }
     
-   function getcount(address car) public view returns(uint r){
-       r = bytes(index[car]).length;
-   }
-    
-    function toString(address x) internal returns (string) {
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
-    }
-    
-    function bytes32ToString (bytes32 data) internal returns (string) {
-        bytes memory bytesString = new bytes(32);
-        for (uint j=0; j<32; j++) {
-            byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[j] = char;
-            }
+    function uint2str(uint i) public pure returns (string){
+        if (i == 0) return "0";
+        uint j = i;
+        uint len;
+        while (j != 0){
+            len++;
+            j /= 10;
         }
-        return string(bytesString);
+        string memory ret = new string(len);
+        uint retptr;
+        uint k = len - 1;
+        assembly { 
+            retptr := add(ret, 32) 
+        }
+        retptr += k;
+        
+        while (i != 0){
+            uint x = 48 + i % 10;
+            assembly {
+                mstore8(retptr, x)
+            }
+            retptr--;
+            i /= 10;
+        }
+        return ret;
     }
-    
-    function strConcat(string _a, string _b) internal returns (string r){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        string memory abcde = new string(_ba.length + _bb.length);
-        bytes memory babcde = bytes(abcde);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        r = string(babcde);
-    }
-    
     
     ///添加维保记录
-    function add_repair(address car, address fhash, string url, uint price, uint stamp) public payable returns (int r) {
-        //assert(bytes(info[car][fhash].url).length != 0);
-        //assert(bytes(url).length == 0);
-        r = 1;
+    function add_repair(address car, address fhash, string url, uint price, uint stamp) public payable {
+        assert(bytes(info[car][fhash].url).length == 0);
+        assert(bytes(url).length != 0);
         info[car][fhash].url = url;
         info[car][fhash].price = price;
         info[car][fhash].seller = msg.sender;
         info[car][fhash].stamp = stamp;
-        r++;
-       if(mkeys[car][fhash] == false){
-            string memory rets;
-           
-           // rets = strConcat(rets, bytes32ToString(bytes32(stamp)));
-            
-            rets = strConcat(rets, ",");
-            //rets = strConcat(rets, toString(msg.sender));
-            rets = strConcat(rets, ",");
-            //rets = strConcat(rets, toString(fhash));
-            rets = strConcat(rets, ",");
-            //rets = strConcat(rets, bytes32ToString(bytes32(price)));
-            rets = strConcat(rets, ";");
-            index[car] = strConcat(index[car], rets);
-            mkeys[car][fhash] = true;
-            r++;
-        }
+        index[car] = index[car].toSlice().concat(uint2str(stamp).toSlice());
+        index[car] = index[car].toSlice().concat(",".toSlice());
+        index[car] = index[car].toSlice().concat(uint2str(price).toSlice());
+        index[car] = index[car].toSlice().concat(";".toSlice());
+        error = 4;
+    }
+    
+    function getaddress(address car) public view returns (string rets){
+        return index[car];
     }
 
     ///添加评分
     function add_rate(address car, address fhash, uint irate, string comment) public payable {
-        assert(bytes(info[car][fhash].url).length == 0);
+        assert(bytes(info[car][fhash].url).length != 0);
         
         info[car][fhash].fallow[msg.sender].v_rate = irate;
         info[car][fhash].fallow[msg.sender].comment = comment;
@@ -109,8 +93,8 @@ contract xintoken {
 
     ///购买维保记录
     function buy(address car, address fhash) public payable {
-        assert(bytes(info[car][fhash].url).length == 0);
-        assert(info[car][fhash].price > coin[msg.sender]);
+        assert(bytes(info[car][fhash].url).length != 0);
+        assert(info[car][fhash].price <= coin[msg.sender]);
 
             
         info[car][fhash].buy[msg.sender] += info[car][fhash].price;
@@ -118,13 +102,9 @@ contract xintoken {
         
         coin[admin] += info[car][fhash].price/10;
         coin[msg.sender] = coin[msg.sender] - info[car][fhash].price;
+        error = 5;
     }
-    
-    ///获取地址
-    function getaddress(address car) public view returns (string rets){
-        return index[car];
-    }
-    
+
     ///4s店检查是否购买过维保记录
     function isbuy(address car, address fhash, address buyer) public view returns (uint){
         if(info[car][fhash].seller != msg.sender)
@@ -145,12 +125,12 @@ contract xintoken {
     
     //提现接口
     function collect() public payable{
-        assert(msg.sender != admin);
+        assert(msg.sender == admin);
         admin.transfer(address(this).balance);
     }
     
     function selfcollect(address car, address seller, uint value) public payable returns (uint){
-        assert(msg.sender != admin);
+        assert(msg.sender == admin);
         info[car][seller].balance -= value;
         return info[car][seller].balance;
     }
