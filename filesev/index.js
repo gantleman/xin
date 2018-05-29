69c677bd0556e48e619f40de236e4b29d95ed485
gantleman <dds_sun@hotmail.com>
2018-05-29 16:17:35 +0800

    filesev

--- filesev/index.js
+++ filesev/index.js
@@ -1,9 +1,33 @@
 //express_demo.js 文件
 var express = require('express');
 var app = express();
+var request = require('superagent')
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+  console.log("hello")
+});
+
+app.post('/api/uppic', function (req, res) {
+  const sreq = request.post(HOST + req.originalUrl);
+  req.body.app = 'blockchain'
+  req.body.key = 'f6070v96i4mI4P5K'
+  sreq.set('Content-Type', 'application/json')
+      .send(req.body)
+
+  sreq.pipe(res);
+  sreq.on('end', function (error, res) {
+    console.log('post end ' + error);
+  });
+});
+
+app.get('/api/getpic', function (req, res) {
+  console.log("get+"+req.query.picurl);
+
+  var sreq = request.get("http://upload.xin.com"+req.query.picurl)
+  sreq.pipe(res);
+  sreq.on('end', function (error, res) {
+    console.log('get end ' + error);
+  });
 })
 
 var server = app.listen(8081, function () {
@@ -11,6 +35,5 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
+  console.log("http://%s:%s", host, port)
 })
